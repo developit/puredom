476bc88ca140c31c100acc18cddfab2894c02ff4
Jason Miller <jason@developit.ca>
2013-10-08 00:24:45 -0400

    Add jshint build step

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,19 +50,39 @@ module.exports = function(grunt) {
 					'dist/<%= pkg.version %>/<%= pkg.name %>.light.min.js': ['<%= concat.light.dest %>']
 				}
 			}
+		},
+		jshint : {
+			options : {
+				browser : true,
+				ignores: [
+					'src/json2.js'
+				]
+			},
+			beforeconcat : [
+				'src/puredom.js',
+				'src/puredom-utils.js',
+				'src/puredom-eventemitter.js',
+				'src/puredom-controllermanager.js',
+				'src/puredom-localstorage.js',
+				'src/**/*.js'
+			],
+			afterconcat: [
+				'<%= concat.full.dest %>'
+			]
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 
 	grunt.registerTask('default', [
+		'jshint:beforeconcat',
 		'concat:light',
 		'uglify:light',
 		'concat:full',
 		'uglify:full'
+		//,'jshint:afterconcat'
 	]);
 	
-	//grunt.registerTask('default', ['concat', 'uglify']);
-
 };
