50039dd52a2bbb2ba509f6099b0878f8e43e9978
Jason Miller <jason@developit.ca>
2014-06-22 19:54:54 -0400

    Dist for 1.2.8

--- dist/puredom.js
+++ dist/puredom.js
@@ -1,7 +1,15 @@
 // json2.js, https://github.com/douglascrockford/JSON-js
 if(!this.JSON){this.JSON={}}(function(){function f(n){return n<10?'0'+n:n}if(typeof Date.prototype.toJSON!=='function'){/**@ignore*/Date.prototype.toJSON=function(key){return isFinite(this.valueOf())?this.getUTCFullYear()+'-'+f(this.getUTCMonth()+1)+'-'+f(this.getUTCDate())+'T'+f(this.getUTCHours())+':'+f(this.getUTCMinutes())+':'+f(this.getUTCSeconds())+'Z':null};/**@ignore*/String.prototype.toJSON=/**@ignore*/Number.prototype.toJSON=/**@ignore*/Boolean.prototype.toJSON=function(key){return this.valueOf()}}var cx=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,escapable=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,gap,indent,meta={'\b':'\\b','\t':'\\t','\n':'\\n','\f':'\\f','\r':'\\r','"':'\\"','\\':'\\\\'},rep;function quote(string){escapable.lastIndex=0;return escapable.test(string)?'"'+string.replace(escapable,function(a){var c=meta[a];return typeof c==='string'?c:'\\u'+('0000'+a.charCodeAt(0).toString(16)).slice(-4)})+'"':'"'+string+'"'}function str(key,holder){var i,k,v,length,mind=gap,partial,value=holder[key];if(value&&typeof value==='object'&&typeof value.toJSON==='function'){value=value.toJSON(key)}if(typeof rep==='function'){value=rep.call(holder,key,value)}switch(typeof value){case'string':return quote(value);case'number':return isFinite(value)?String(value):'null';case'boolean':case'null':return String(value);case'object':if(!value){return'null'}gap+=indent;partial=[];if(Object.prototype.toString.apply(value)==='[object Array]'){length=value.length;for(i=0;i<length;i+=1){partial[i]=str(i,value)||'null'}v=partial.length===0?'[]':gap?'[\n'+gap+partial.join(',\n'+gap)+'\n'+mind+']':'['+partial.join(',')+']';gap=mind;return v}if(rep&&typeof rep==='object'){length=rep.length;for(i=0;i<length;i+=1){k=rep[i];if(typeof k==='string'){v=str(k,value);if(v){partial.push(quote(k)+(gap?': ':':')+v)}}}}else{for(k in value){if(Object.hasOwnProperty.call(value,k)){v=str(k,value);if(v){partial.push(quote(k)+(gap?': ':':')+v)}}}}v=partial.length===0?'{}':gap?'{\n'+gap+partial.join(',\n'+gap)+'\n'+mind+'}':'{'+partial.join(',')+'}';gap=mind;return v}}if(typeof JSON.stringify!=='function'){/**@ignore*/JSON.stringify=function(value,replacer,space){var i;gap='';indent='';if(typeof space==='number'){for(i=0;i<space;i+=1){indent+=' '}}else if(typeof space==='string'){indent=space}rep=replacer;if(replacer&&typeof replacer!=='function'&&(typeof replacer!=='object'||typeof replacer.length!=='number')){throw new Error('JSON.stringify');}return str('',{'':value})}}if(typeof JSON.parse!=='function'){/**@ignore*/JSON.parse=function(text,reviver){var j;/**@ignore*/function walk(holder,key){var k,v,value=holder[key];if(value&&typeof value==='object'){for(k in value){if(Object.hasOwnProperty.call(value,k)){v=walk(value,k);if(v!==undefined){value[k]=v}else{delete value[k]}}}}return reviver.call(holder,key,value)}text=String(text);cx.lastIndex=0;if(cx.test(text)){text=text.replace(cx,function(a){return'\\u'+('0000'+a.charCodeAt(0).toString(16)).slice(-4)})}if(/^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,'@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,']').replace(/(?:^|:|,)(?:\s*\[)+/g,''))){j=eval('('+text+')');return typeof reviver==='function'?walk({'':j},''):j}throw new SyntaxError('JSON.parse');}}}());
-(function() {
+(function(window, global) {
 	/**	@exports self as puredom */
+
+	// node:
+	if (typeof process==='object' && process.argv && process.argv[0]==='node') {
+		window = require('jsdom').jsdom().parentWindow;
+	}
+
+	var document = window.document,
+		navigator = window.navigator;
 	
 	var previousSelf = window.puredom;
 	
@@ -4162,8 +4170,9 @@ if(!this.JSON){this.JSON={}}(function(){function f(n){return n<10?'0'+n:n}if(typ
 	/**	Log to the browser console, if it exists.
 	 */
 	self.log = function() {
-		if (window.console && window.console.log) {
-			window.console.log.apply(window.console, arguments);
+		var c = global.console;
+		if (c && c.log) {
+			c.log.apply(c, arguments);
 		}
 	};
 	
@@ -4244,11 +4253,14 @@ if(!this.JSON){this.JSON={}}(function(){function f(n){return n<10?'0'+n:n}if(typ
 	self.extend(self, baseSelf);
 	self.toString = function(){return 'function puredom(){}';};
 	
-	window.puredom = self;
-	if (typeof window.define==='function' && window.define.amd) {
+	this.puredom = window.puredom = self;
+	if (typeof define==='function' && define.amd) {
 		window.define('puredom', function(){ return self; });
 	}
-}());
+	if (typeof module==='object') {
+		module.exports = self;
+	}
+}(this, typeof global==='object' ? global : this));
 /**	@fileOverview Utilities that just get grafted onto the puredom namespace. */
 
 puredom.extend(puredom, /** @lends puredom */ {
